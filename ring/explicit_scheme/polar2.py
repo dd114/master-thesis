df100b676466bf57ae8e1e45091552edefa999e1
dd114 <my_email@mail.com>
2025-03-09 21:50:15 +0300

    computation has been optimized

--- ring/explicit_scheme/polar2.py
+++ ring/explicit_scheme/polar2.py
@@ -12,8 +12,8 @@ R = 1.0           # Радиус круга
 c = 1.0           # Скорость волны
 # Nr = 200           # Число узлов по rad
 # Nphi = 300         # Число узлов по φ
-Nr = 100           # Число узлов по rad
-Nphi = 200         # Число узлов по φ
+Nr = 150           # Число узлов по rad
+Nphi = 250         # Число узлов по φ
 dr = R / (Nr - 1) # Шаг по rad
 dphi = 2 * np.pi / Nphi  # Шаг по φ
 dt = 0.99 * min(dr, dphi, r, dphi * r) / (c * np.sqrt(2))  # Шаг по времени (условие Куранта); np.sqrt(3)?
@@ -41,29 +41,57 @@ expect = 0.8
 sigma2 = 0.001
 
 # Начальные условия (пример: гауссов импульс)
-def initial_f(rad, phi):
+def initial_state(rad, phi):
     # return 0.1 * np.exp( - ((rad - 0.9) ** 2) / (2 * 0.001))
-    return 0.5 * np.exp( - ((rad - 0.9) ** 2) / (2 * 0.001)) * np.exp( - ((phi - np.pi) ** 2) / (2 * 0.001))
+    return 0.5 * np.exp( - ((rad - 0.9) ** 2) / (2 * 0.001)) * np.cos(20 * phi)
+    # return 0.5 * np.exp( - ((rad - 0.9) ** 2) / (2 * 0.001)) * np.exp( - ((phi - np.pi) ** 2) / (2 * 0.001))
+
     # return np.exp(-50*(rad - 0.3)**2) * 0.5 * np.sin(phi)
     # return -np.exp(-50*(rad - 0.3)**2)
 
-u_prev = initial_f(Rad_grid, Phi_grid)
+    return np.zeros_like(rad)
+
+# Начальные условия (пример: гауссов импульс)
+def initial_speed(rad, phi):
+    # return -1 * np.exp( - ((rad - 0.9) ** 2) / (2 * 0.001))
+    # return 5 * np.exp( - ((rad - 0.9) ** 2) / (2 * 0.001)) * np.cos(20 * phi)
+    # return 0.5 * np.exp( - ((rad - 0.9) ** 2) / (2 * 0.001)) * np.exp( - ((phi - np.pi) ** 2) / (2 * 0.001))
+    
+    # return np.exp(-50*(rad - 0.3)**2) * 0.5 * np.sin(phi)
+    # return -np.exp(-50*(rad - 0.3)**2)
+
+    return np.zeros_like(rad)
+
+u_prev = initial_state(Rad_grid, Phi_grid)
 
 # Граничные условия при rad=R и rad=r
 u_prev[0, :] = 0.0
 u_prev[-1, :] = 0.0
 
-u_curr[:, :] = u_prev[:, :]
+speed = initial_speed(Rad_grid, Phi_grid)
+
+u_curr[:, :] = u_prev[:, :] + speed * dt
 
 X = Rad_grid * np.cos(Phi_grid)
 Y = Rad_grid * np.sin(Phi_grid)
 
 
-# Начальные условия
+# Начальное состояние
 fig = plt.figure(figsize=(12, 10))
 ax = fig.add_subplot(111, projection='3d')
-ax.set_title(f'Начальные условия', fontsize=14, fontweight="bold")
-surface = ax.plot_surface(X, Y, u_curr, cmap='viridis')
+ax.set_title(f'Начальное состояние', fontsize=14, fontweight="bold")
+surface = ax.plot_surface(X, Y, u_prev, cmap='viridis')
+ax.set_zlim(-1, 1)
+
+ax.set_xlabel("X", fontsize=14, fontweight="bold")
+ax.set_ylabel("Y", fontsize=14, fontweight="bold")
+ax.set_zlabel("U", fontsize=14, fontweight="bold")
+
+# Начальная скорость
+fig = plt.figure(figsize=(12, 10))
+ax = fig.add_subplot(111, projection='3d')
+ax.set_title(f'Начальная скорость', fontsize=14, fontweight="bold")
+surface = ax.plot_surface(X, Y, speed, cmap='viridis')
 ax.set_zlim(-1, 1)
 
 ax.set_xlabel("X", fontsize=14, fontweight="bold")
@@ -76,33 +104,65 @@ ax = fig.add_subplot(111, projection='3d')
 # surface = ax.plot_surface(X, Y, u_curr, cmap='viridis')
 ax.set_zlim(-1, 1)
 
+d2u_dr2 = np.zeros((Nr, Nphi))
+du_dr = np.zeros((Nr, Nphi))
+radial = np.zeros((Nr, Nphi))
+
+d2u_dphi2 = np.zeros((Nr, Nphi))
+angular = np.zeros((Nr, Nphi))
 
 # Функция обновления кадра
 def update(frame):
     global u_prev, u_curr, t_curr
+    global d2u_dr2, du_dr, radial
+    global d2u_dphi2, angular
     
     u_next = np.zeros((Nr, Nphi))
+
+    # TEST
     
     # Вычисление нового слоя
-    for i in range(1, Nr-1):
-        for j in range(Nphi):
-            j_prev = (j - 1) % Nphi
-            j_next = (j + 1) % Nphi
-            
-            # Радиальная часть
-            d2u_dr2 = (u_curr[i+1,j] - 2*u_curr[i,j] + u_curr[i-1,j]) / dr**2
-            du_dr = (u_curr[i+1,j] - u_curr[i-1,j]) / (2 * dr)
-            radial = d2u_dr2 + du_dr / (rad[i])
-            
-            # Угловая часть
-            d2u_dphi2 = (u_curr[i,j_next] - 2*u_curr[i,j] + u_curr[i,j_prev]) / dphi**2
-            angular = d2u_dphi2 / (rad[i]**2)
-            
-            # Обновление
-            u_next[i,j] = 2*u_curr[i,j] - u_prev[i,j] + (c**2 * dt**2) * (radial + angular)
+    for i in range(1, Nr - 1):
+        # Радиальная часть
+        d2u_dr2[i, :] = (u_curr[i+1, :] - 2*u_curr[i, :] + u_curr[i-1, :]) / dr**2
+        du_dr[i, :] = (u_curr[i+1, :] - u_curr[i-1, :]) / (2 * dr)
+        radial[i, :] = d2u_dr2[i, :] + du_dr[i, :] / (rad[i])
 
-            # if np.abs(u_next).max() > 1:
-            #     print(np.abs(u_next).max())
+    for j in range(Nphi):
+        # Угловая часть
+        j_prev = (j - 1) % Nphi
+        j_next = (j + 1) % Nphi
+
+        d2u_dphi2[:, j] = (u_curr[: ,j_next] - 2*u_curr[: ,j] + u_curr[: ,j_prev]) / dphi**2
+        angular[:, j] = d2u_dphi2[:, j] / (rad[:]**2)
+
+
+    # Обновление
+    u_next[1:Nr-1, :] = 2*u_curr[1:Nr-1, :] - u_prev[1:Nr-1, :] + (c**2 * dt**2) * (radial[1:Nr-1, :] + angular[1:Nr-1, :])
+
+
+    # TEST
+
+
+    # for i in range(1, Nr-1):
+    #     for j in range(Nphi):
+    #         j_prev = (j - 1) % Nphi
+    #         j_next = (j + 1) % Nphi
+            
+    #         # Радиальная часть
+    #         d2u_dr2 = (u_curr[i+1,j] - 2*u_curr[i,j] + u_curr[i-1,j]) / dr**2
+    #         du_dr = (u_curr[i+1,j] - u_curr[i-1,j]) / (2 * dr)
+    #         radial = d2u_dr2 + du_dr / (rad[i])
+            
+    #         # Угловая часть
+    #         d2u_dphi2 = (u_curr[i,j_next] - 2*u_curr[i,j] + u_curr[i,j_prev]) / dphi**2
+    #         angular = d2u_dphi2 / (rad[i]**2)
+            
+    #         # Обновление
+    #         u_next[i,j] = 2*u_curr[i,j] - u_prev[i,j] + (c**2 * dt**2) * (radial + angular)
+
+    #         # if np.abs(u_next).max() > 1:
+    #         #     print(np.abs(u_next).max())
     
     # Граничные условия при rad=R
     u_next[-1, :] = 0.0
@@ -120,8 +180,8 @@ def update(frame):
     #     u_next[0, :] = avg  # Усреднение для rad=0
     
     # Обновление временных слоев
-    u_prev[:, :] = u_curr
-    u_curr[:, :] = u_next
+    u_prev = u_curr[:, :]
+    u_curr = u_next[:, :]
     
     # Обновление графика
     ax.clear()
