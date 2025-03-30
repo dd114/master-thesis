cf56d6580be77a0980826fcb24515c566dab2134
dd114 <my_email@mail.com>
2025-03-30 23:31:15 +0300

    1st version of implicit polar solution in ring

---
+++ ring/implicit_scheme/my_polar.py
@@ -0,0 +1,144 @@
+import numpy as np
+import scipy.sparse as sp
+import scipy.sparse.linalg as splinalg
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+# Параметры задачи
+R = 1.0           # Радиус круга
+Nr = 51            # Количество узлов по r
+Nphi = 51            # Количество узлов по phi
+c = 1.0           # Скорость волны
+sigma = 0.2       # Параметр начального гауссова распределения
+t_steps = 100     # Количество временных шагов
+dt = 0.02         # Шаг по времени
+output_step = 1   # Шаг для вывода анимации (уменьшает количество кадров)
+
+# Сетка
+rad = np.linspace(0.1, R, Nr)
+phi = np.linspace(0, 2*np.pi, Nphi)
+Rad_grid, Phi_grid = np.meshgrid(rad, phi, indexing='ij')
+
+X = Rad_grid * np.cos(Phi_grid)
+Y = Rad_grid * np.sin(Phi_grid)
+
+
+# Создание сетки
+# x = np.linspace(-R, R, N)
+# y = np.linspace(-R, R, N)
+# X, Y = np.meshgrid(x, y)
+dr = rad[1] - rad[0]  # Шаг сетки
+dphi = phi[1] - phi[0]  # Шаг сетки
+
+# Маска для внутренних точек круга
+# mask = ((X**2 + Y**2) <= R**2) * ((X**2 + Y**2) >= 0.1)
+
+# Начальные условия (гауссов импульс в центре)
+u0 = 0.5 * np.exp( - ((Rad_grid - 0.9) ** 2) / (2 * 0.001)) 
+
+
+# Создание индексов для внутренних точек
+indices = np.full((Nr, Nphi), -1, dtype=int)
+current_idx = 0
+for i in range(Nr):
+    for j in range(Nphi):
+        indices[i, j] = current_idx
+        current_idx += 1
+
+M = current_idx  # Число внутренних точек
+
+# Параметр устойчивости
+# s = (c * dt / h)**2
+s = (c * dt)
+
+# Построение матрицы системы
+A = sp.lil_matrix((M, M))
+
+for i in range(Nr):
+    for j in range(Nphi):
+        k = indices[i, j]
+        A[k, k] = 1 + 2 * ((s / dr) ** 2) + 2 * (s / (rad[i] * dphi)) ** 2
+
+        # Соседи
+
+        pi, pj, ni, nj = i + 1, (j + 1) % Nphi, i - 1, (j - 1) % Nphi
+
+        if 0 <= pi < Nr:
+            cur_u = indices[pi, j]
+            A[k, cur_u] = - (s / dr) ** 2 - (s ** 2) / (2 * rad[i] * dr)
+
+        if 0 <= ni < Nr:
+            cur_u = indices[ni, j]
+            A[k, cur_u] = - (s / dr) ** 2 + (s ** 2) / (2 * rad[i] * dr)
+
+        # if 0 <= pj < Nphi:
+        cur_u = indices[i, pj]
+        A[k, cur_u] = - (s / (rad[i] * dphi)) ** 2
+
+        # if 0 <= nj < Nphi:
+        cur_u = indices[i, nj]
+        A[k, cur_u] = - (s / (rad[i] * dphi)) ** 2
+
+
+        # for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        #     ni, nj = i + di, j + dj
+        #     if 0 <= ni < N and 0 <= nj < N and mask[ni, nj]:
+        #         nk = indices[ni, nj]
+        #         A[k, nk] = -s
+
+# # reference
+# for i in range(N):
+#     for j in range(N):
+#         if mask[i, j]:
+#             k = indices[i, j]
+#             A[k, k] = 1 + 4*s
+#             # Соседи
+#             for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+#                 ni, nj = i + di, j + dj
+#                 if 0 <= ni < N and 0 <= nj < N and mask[ni, nj]:
+#                     nk = indices[ni, nj]
+#                     A[k, nk] = -s
+A = A.tocsr()
+
+u_prev = u0.flatten().astype(np.float64)
+
+# # Вычисление первого шага с использованием начальной скорости (здесь нулевая)
+# laplacian_u0 = np.zeros_like(u0)
+# for i in range(1, N-1):
+#     for j in range(1, N-1):
+#         if mask[i, j]:  
+#             laplacian_u0[i, j] = (u0[i+1, j] - 2*u0[i, j] + u0[i-1, j])/h**2 + \
+#                                 (u0[i, j+1] - 2*u0[i, j] + u0[i, j-1])/h**2
+# laplacian_u0_flat = laplacian_u0[mask].flatten()
+
+u_curr = u_prev.copy()
+
+# Сохранение истории для анимации
+U_history = []
+
+# Временной цикл
+for n in range(1, t_steps):
+    print(u0.shape, u_prev.shape, u_curr.shape)
+
+    b = 2 * u_curr - u_prev
+    u_next = splinalg.spsolve(A, b)
+    print(u0.shape, u_prev.shape, u_curr.shape, u_next.shape)
+    u_prev, u_curr = u_curr, u_next
+
+    # u_grid = np.zeros((Nr, Nphi))
+    # u_grid = u_curr[:]
+    U_history.append(u_curr.reshape((Nr, Nphi)))
+
+# Создание анимации
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_zlim(-1, 1)
+
+def update(frame):
+    ax.clear()
+    ax.set_zlim(-1, 1)
+    surf = ax.plot_surface(X, Y, U_history[frame], cmap='viridis', rstride=1, cstride=1)
+    return surf,
+
+ani = animation.FuncAnimation(fig, update, frames=len(U_history), interval=50, blit=False)
+plt.show()
