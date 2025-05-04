7375082c4367b9e048e8823d1c44d62def8ea594
dd114 <my_email@mail.com>
2025-05-04 16:29:51 +0300

    feature: leak accounting and sliding window

--- draw_dependencies.py
+++ draw_dependencies.py
@@ -27,8 +27,12 @@ if __name__ == '__main__':
     print(np.load(f'frequently_results/gallery_score_w{n}.npy'))
     # plt.plot(np.load(f'frequently_results/time_series.npy'), np.load(f'frequently_results/gallery_score_w{n}.npy'))
 
+    window = 20
+
     for w in range(10, 100, 10):
         current = np.load(f'frequently_results/gallery_score_w{w}.npy') 
+        current = np.convolve(current, np.ones(window)/window, 'same')
+
         plt.plot(time_arr, current, label=f'w{w}')
 
     plt.legend()
@@ -45,11 +49,14 @@ if __name__ == '__main__':
     plt.legend()
     plt.show()
 
+    window = 10
 
     for w in [90]: 
         for a in [0.05, 0.15, 0.3]:
             for b in [0.05, 0.25, 0.5]:
                 current = np.load(f'm_ellipse_results/gallery_score_w{w}_a{a}_b{b}.npy')
+                current = np.convolve(current, np.ones(window)/window, 'same')
+
                 plt.plot(time_arr, current, label=f'w{w}_a{a}_b{b}')
     
     plt.title('ellipse params')
@@ -57,4 +64,33 @@ if __name__ == '__main__':
     plt.legend()
     plt.show()
 
-    print("qqqq")
+    fig = plt.figure(figsize=(10, 10))
+
+    ax1 = fig.add_subplot(111)
+    # ax2 = fig.add_subplot(122)
+
+    window = 50  # Размер окна для скользящего среднего.
+
+    for w in range(10, 102, 10):
+        time_arr = np.load(f'leaking_out_circle_score/time_series.npy')
+        current = np.load(f'leaking_out_circle_score/leaking_out_circle_score_w{w}.npy')
+
+        # Применяем скользящее среднее.
+        current = np.convolve(current, np.ones(window)/window, 'same')
+
+        ax1.plot(time_arr, current, '.', label=f'w{w}')
+        
+
+        time_arr = np.load(f'old/leaking_out_circle_score__0.1r2/time_series.npy')
+        current = np.load(f'old/leaking_out_circle_score__0.1r2/leaking_out_circle_score_w{w}.npy')
+
+        # Применяем скользящее среднее.
+        current = np.convolve(current, np.ones(window)/window, 'same')
+
+        # ax2.plot(time_arr, current, '.', label=f'w{w}')
+    
+    plt.title('leaking')
+
+    plt.legend()
+    plt.show()
+
